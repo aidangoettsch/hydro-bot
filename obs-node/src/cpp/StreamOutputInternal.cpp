8ac58dc9fc473ddf7d307f45bbd5712e2cc1eedd
Aidan Goettsch <aidangoettsch@gmail.com>
2023-11-30 15:00:01 -0500

    Update StreamOutputInternal.cpp

--- obs-node/src/cpp/StreamOutputInternal.cpp
+++ obs-node/src/cpp/StreamOutputInternal.cpp
@@ -1,5 +1,9 @@
 #include "StreamOutputInternal.h"
 
+/**
+ * Constructor for StreamOutputInternal. This class is responsible for interacting
+ * with OBS to pretend to be a streaming service and pass video data into Node.js.
+ */
 StreamOutputInternal::StreamOutputInternal(
   obs_output_t *output,
   Napi::ThreadSafeFunction* onData,
@@ -7,7 +11,10 @@ StreamOutputInternal::StreamOutputInternal(
   Napi::ObjectReference* jsThis,
   Napi::AsyncContext* asyncContext
 ) {
+  // output is a pointer to the OBS API struct representing this output
   this->output = output;
+  // The other member variables are the Node.js callbacks and the information
+  // needed to call them.
   this->onData = onData;
   this->onStop = onStop;
   this->jsThis = jsThis;
@@ -22,6 +29,10 @@ const char* StreamOutputInternal::GetName([[maybe_unused]] void* typeData) {
   return outputName;
 }
 
+/**
+ * Create a new StreamOutputInternal. This is a static function enrolled with OBS to
+ * be called when StreamOutput requests the output be created.
+ */
 void* StreamOutputInternal::Create(obs_data_t *settings, obs_output_t *output) {
   long long onData = obs_data_get_int(settings, "onData");
   long long onStop = obs_data_get_int(settings, "onStop");
@@ -38,10 +49,17 @@ void* StreamOutputInternal::Create(obs_data_t *settings, obs_output_t *output) {
   return data;
 }
 
+/**
+ * Destroy a StreamOutputInternal. This is a static function enrolled with OBS to
+ * be called when the output should be freed.
+ */
 void StreamOutputInternal::Destroy(void* data) {
   delete reinterpret_cast<StreamOutputInternal *>(data);
 }
 
+/**
+ * Start the output by calling the OBS API.
+ */
 bool StreamOutputInternal::Start(void* data) {
   auto output = (StreamOutputInternal*)(data);
 
@@ -54,6 +72,9 @@ bool StreamOutputInternal::Start(void* data) {
   return obs_output_begin_data_capture(output->output, 0);
 }
 
+/**
+ * Stop the output by calling the OBS API.
+ */
 void StreamOutputInternal::Stop(void* data, [[maybe_unused]] uint64_t ts) {
   auto output = (StreamOutputInternal*)(data);
   obs_output_end_data_capture(output->output);
@@ -63,22 +84,38 @@ void StreamOutputInternal::Stop(void* data, [[maybe_unused]] uint64_t ts) {
   }
 }
 
+/**
+ * 
+ */
 void StreamOutputInternal::OnPacket(void* data, encoder_packet *packet) {
   auto output = (StreamOutputInternal*)(data);
 
   if (output->onData != nullptr) {
+    // Packets are reference counted objects, so create a new reference for this packet.
     auto packetRef = new encoder_packet();
     obs_encoder_packet_ref(packetRef, packet);
-    output->onData->BlockingCall(packetRef, []( Napi::Env env, Napi::Function jsCallback, encoder_packet *data ) {
+
+    // Call the onData function in Node.js. The lambda is responsible for actually performing the call
+    // with access to the environment of the function, which is required to create objects that are properly
+    // tracked by the runtime.
+    output->onData->BlockingCall(packetRef, [](Napi::Env env, Napi::Function jsCallback, encoder_packet *data) {
+      // Create a Node.js Buffer and copy data into it
       auto array = Napi::ArrayBuffer::New(env, data->size);
       memcpy(array.Data(), data->data, data->size);
+      
+      // Do the actual function call, passing the data and the type of the packet.
       jsCallback.Call( {array, Napi::Number::New(env, data->type)} );
+
+      // Release the reference we hold on the packet
       obs_encoder_packet_release(data);
       delete data;
     });
   }
 }
 
+/**
+ * Update callbacks if they have changed.
+ */
 void StreamOutputInternal::Update(void* data, obs_data_t* settings) {
   auto output = (StreamOutputInternal*)(data);
   long long onData = obs_data_get_int(settings, "onData");
